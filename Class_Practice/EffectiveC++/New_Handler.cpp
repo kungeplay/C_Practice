7baba7efae3b3fa49ef775079fcb4803b85115ca
kungeplay <kungeplay@gmail.com>
2015-05-23 19:25:08 +0800

    在Effective C++中看到了重载new的操作，很感兴趣，写了写，但并不完整。留着下次再改吧

--- Class_Practice/EffectiveC++/New_Handler.cpp
+++ Class_Practice/EffectiveC++/New_Handler.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
 #include <stdlib.h>
+// #include <new>
 //条款49：了解new-handler的行为
 //当operator new无法满足某一内存分配需求时，它会抛出异常.
 //set_new_handler的参数是个指针，指向operator new无法分配足够内存时该被调
 //用的函数。其返回值也是个指针，指向set_new_handler被调用前正在执行(但马上就要被替换)的那个new-handler函数.
+//注意当operator new无法满足内存申请时，它会不断调用new-handler函数，直到找到足够内存。
+//因此一个设计良好的new-handler函数必须做以下事情：1.让更多内存可被使用；2.安装另一个new-handler；3.卸除new-handler;4.抛出bad_alloc(或派生自bad_alloc)的异常；
+//5. 不返回；
 using namespace std;
-void OutOfMem()
+void OutOfMem()//
 {
 	std::cerr<<"Unable to satisfy reuest for memory\n";
-	abort();
+	//abort();
+	throw std::bad_alloc();
 }
 int main(int argc, char const *argv[])
 {
-	std::set_new_handler(OutOfMem);
-	int * pBigDataArray=new int[1000000000000000000];
+	//下面两部分代码要注释其中一方
+	 std::set_new_handler(OutOfMem);
+	 int * pBigDataArray1=new int[1000000000000000000];
+	
+ 
+	// std::set_new_handler(0);
+	// int * pBigDataArray2=new(nothrow) int[1000000000000000000];//默认情况下如果不在new参数中添加加nothrow，则直接抛出异常,退出程序执行.
+	// if (pBigDataArray2==0)
+	// {
+	// 	std::cerr<<"out of memory"<<endl;
+	// }
 	return 0;
 }
---
+++ Class_Practice/EffectiveC++/New_Handler_Class.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+//条款49，在类中重载了new操作符。但在这里并没有完全重载,可以看P249和P253页重载方法
+//这个代码其实写的并不完整，可以参见条款50，51部分.
+//类NewHandlerHolder的作用是保存旧的new_handler，然后在析构函数中恢复,比较方便
+class NewHandlerHolder
+{
+public:
+	explicit NewHandlerHolder(std::new_handler nh):handler(nh)
+	{
+		std::cout<<"NewHandlerHolder"<<std::endl;
+	}
+	~NewHandlerHolder()
+	{
+		std::cout<<"~NewHandlerHolder"<<std::endl;
+		std::set_new_handler(handler);
+	}
+private:
+	std::new_handler handler;
+	NewHandlerHolder(const NewHandlerHolder &)=delete;
+	NewHandlerHolder & operator =(const NewHandlerHolder &)=delete;
+};
+
+//CRTP技术,用以支持class专属的set_new_handler
+template <typename T>
+class NewHandlerSupport
+{
+public:
+	NewHandlerSupport()
+	{ 	
+		std::cout<<"NewHandlerSupport"<<std::endl;
+	}
+	virtual ~NewHandlerSupport()
+	{
+		std::cout<<"~NewHandlerSupport"<<std::endl;
+	}
+	static std::new_handler set_new_handler(std::new_handler p) throw();
+	static void * operator new(std::size_t size) throw(std::bad_alloc);
+	
+private:
+	static std::new_handler currentHandler;
+};
+template<typename T> std::new_handler NewHandlerSupport<T>::set_new_handler(std::new_handler p) throw()
+{
+	std::new_handler oldHandler=currentHandler;
+	currentHandler=p;
+	return oldHandler;
+}
+template<typename T> void * NewHandlerSupport<T>::operator new(std::size_t size) throw(std::bad_alloc)
+{
+	std::cout<<"NewHandlerSupport<T> new \tsize="<<size<<"\tsizeof(NewHandlerSupport<T>)="<<sizeof(NewHandlerSupport<T>)<<std::endl;
+	NewHandlerHolder h(std::set_new_handler(currentHandler));
+	return ::operator new(size);
+}
+template <typename T> std::new_handler NewHandlerSupport<T>::currentHandler=0;
+
+
+class Widget:public NewHandlerSupport<Widget>
+{
+public:
+	Widget()
+	{
+
+		std::cout<<"Widget\t"<<sizeof(Widget)<<std::endl;
+	}
+	~Widget()
+	{
+		std::cout<<"~Widget"<<std::endl;
+	}
+	int w;
+};
+
+
+void outOfMem()
+{
+	std::cerr<<"Unable to satisfy request for memory\n"<<std::endl;
+	std::abort();
+}
+
+
+int main(int argc, char const *argv[])
+{
+	NewHandlerSupport<Widget>::set_new_handler(outOfMem);
+	Widget *p=new Widget;
+	delete p;
+	return 0;
+}
