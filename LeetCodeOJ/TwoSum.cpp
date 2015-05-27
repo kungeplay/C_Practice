7f2ca7ffabc49264234b0ff05800bd4a97aef9b2
kungeplay <kungeplay@gmail.com>
2015-05-27 18:52:11 +0800

    解决了LeetCode OJ上的第一道题目Two Sum，这个题比较难

--- LeetCodeOJ/TwoSum.cpp
+++ LeetCodeOJ/TwoSum.cpp
@@ -5,29 +5,57 @@
 // You may assume that each input would have exactly one solution.
 // Input: numbers={2, 7, 11, 15}, target=9
 // Output: index1=1, index2=2 
+// 这个题我刚开始是想来个循环里套循环，此时时间复杂度为O(n^2),提交后提示超时，后来在网上查到map底层使用红黑树实现的，所以它的查找时间是O(logN)
 #include <iostream>
 #include <vector>
+#include <map>
 using namespace std;
 
 class Solution {
 public:
-	//暂时想到的最简单的方法
     vector<int> twoSum(vector<int>& nums, int target) {
+		std::vector<int> v;    	
     		int len=nums.size();
+    		std::map<int,int> mapcontainer;
   		for (int i = 0; i < len; ++i)
-		{
-			for (int j = i+1; j < len; ++j)
+		{	
+			//若当前元素没有插入到map中，则插入到map表里，key为元素值，value为元素索引。
+			if(mapcontainer.find(nums[i])==mapcontainer.end())
+				mapcontainer.insert(make_pair(nums[i],i));
+			else if(nums[i]*2==target)//若碰到重复元素，则直接判断
 			{
-				if (nums[i]+nums[j]==target)
+				if (i<mapcontainer.at(nums[i]))
 				{
-					std::vector<int> sumv;
-					sumv.push_back(i+1);
-					sumv.push_back(j+1);
-					return sumv;
+					v.push_back(i+1);
+					v.push_back(mapcontainer.at(nums[i])+1);									
 				}
+				else
+				{
+					v.push_back(mapcontainer.at(nums[i])+1);
+					v.push_back(i+1);
+				}
+
+				return v;
 			}
-			
-		}      
+		}
+		for(auto iter=mapcontainer.begin();iter!=mapcontainer.end();++iter)
+		{
+			if (mapcontainer.find(target-iter->first)!=mapcontainer.end())//map底层使用红黑树实现的，所以它的查找时间是O(logN)
+			{
+				if (iter->second<mapcontainer.at(target-iter->first))
+				{
+					v.push_back(iter->second+1);
+					v.push_back(mapcontainer.at(target-iter->first)+1);
+				}
+				else
+				{
+					v.push_back(mapcontainer.at(target-iter->first)+1);
+					v.push_back(iter->second+1);					
+				}
+				return v;
+			}
+		}
+		return v;
     }
 };
 
