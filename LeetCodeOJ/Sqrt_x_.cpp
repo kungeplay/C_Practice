d3c03a902e289dbcb14b637c6b4694302185d3fd
kungeplay <kungeplay@gmail.com>
2015-07-24 10:28:20 +0800

    求数的平方根的问题，我竟然没想到要用二分查找的方法，在网上看到还有人用牛顿迭代法，连名字都没听说过，回头要再看一看

---
+++ LeetCodeOJ/Sqrt_x_.cpp
@@ -0,0 +1,36 @@
+// Sqrt(x)(平方根) 
+// mplement int sqrt(int x).
+
+// Compute and return the square root of x.
+
+#include <iostream>
+using namespace std;
+//这个题我不会，从网上找到的二分查找方法，要注意变量类型都定义long类型，尤其是mid和sum;
+class Solution {
+public:
+    int mySqrt(int x) {
+		long long left=1;
+		long long right=x;
+		while(left<=right)
+		{
+			long long mid=(left+right)/2;
+			long long sum=mid*mid;
+			if(sum<x)
+				left=mid+1;
+			else if(sum>x)
+				right=mid-1;
+			else
+				return mid;
+		}
+		return right;
+    }
+};
+int main(int argc, char const *argv[])
+{
+	Solution so;
+	cout<<so.mySqrt(2147395599)<<endl;
+	long long a;
+	long b;
+	cout<<sizeof(a)<<'\t'<<sizeof(b)<<endl;
+	return 0;
+}
